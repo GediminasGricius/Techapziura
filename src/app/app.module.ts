4596f9497c8481c3aa29ecab1f8beb459fd14872
GediminasGricius <g.gricius@ituostas.lt>
2022-11-15 18:50:17 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationNewComponent } from './components/registration-new/registration-new.component';
+import {FormsModule} from "@angular/forms";
+import { CarYearDirective } from './directives/car-year.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationNewComponent,
+    CarYearDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
